ec68a77fa85e07e691b17afdaa8a691743cb8e47
User1 <yason.grace@mail.ru>
2023-02-17 19:07:04 +0300

    first commit

---
+++ app/src/main/java/ru/itlab/todolist/MainActivity.java
@@ -0,0 +1,54 @@
+package ru.itlab.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Random random;
+    private List<Note> notes;
+    private LayoutInflater layoutInflater;
+    private LinearLayoutCompat linearLayoutCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linearLayoutCompat = findViewById(R.id.linearLayout);
+
+        random = new Random();
+        notes = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            notes.add(new Note(random.nextInt(3), "Note: " + i));
+        }
+
+        layoutInflater = getLayoutInflater();
+
+        for (int i = 0; i < notes.size(); i++) {
+            TextView note = (TextView) layoutInflater.inflate(R.layout.note, null, false);
+            note.setText(notes.get(i).text);
+            switch (notes.get(i).priority){
+                case 0:
+                    note.setBackgroundColor(getResources().getColor(R.color.green));
+                    break;
+                case 1:
+                    note.setBackgroundColor(getResources().getColor(R.color.yellow));
+                    break;
+                case 2:
+                    note.setBackgroundColor(getResources().getColor(R.color.red));
+                    break;
+            }
+        }
+
+    }
+}
